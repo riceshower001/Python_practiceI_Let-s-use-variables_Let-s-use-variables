8a55d9c02329a51aace6a459d27693beb979f2be
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-03 13:46:20 +0900

    Add files via upload

---
+++ Python_practiceI_Let-s-use-variables_Let-s-use-variables.py
@@ -0,0 +1,9 @@
+# Please define the variables "apple_price" and "apple_count".
+apple_price = 200
+apple_count = 8
+
+# Please assign the result of multiplying "apple_price" and "apple_count" to the variable "total_price".
+total_price = apple_price * apple_count
+
+# Please print the value of "total_price".
+print(total_price)
